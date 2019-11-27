c1e86b257b0ad2ca5650f2ca3b5c784e21616d95
Matt Helm <mthelm85@gmail.com>
2019-11-27 13:23:19 -0600

    redfined df/tbl so that dir can be removed

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,14 +3,16 @@ using DataDeps
 using DataFrames
 using Test
 
+df = prepdata(2019, ["HRINTSTA"])
+tbl = prepdata(2019, ["HRINTSTA"], indexedtable=true, dir=joinpath(pwd(), "test", "data"))
+
 @testset "CurrentPopulationSurvey.jl" begin
-    df = prepdata(2019, ["HRINTSTA"])
     @test isdir(@datadep_str "CPS 2019")
     @test typeof(df) == DataFrames.DataFrame
     @test in(:hrintsta, names(df))
-
-    prepdata(2019, ["HRINTSTA"], indexedtable=true, dir="./test/data")
-    @test isfile("./test/data/CPS 2019")
+    @test isfile(joinpath(pwd(), "test", "data", "CPS 2019"))
 end
 
-rm("./test/data/", recursive=true)
+df = Nothing
+tbl = Nothing
+rm(joinpath(pwd(), "test", "data"), recursive=true)
