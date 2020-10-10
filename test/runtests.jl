ec9a7a0fbd97dcee3336762ad03b4e9e9184fb00
Matt Helm <mthelm85@gmail.com>
2020-10-10 15:47:33 -0500

    fixed test

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,7 @@ using Test
     tbl = cpsdata(2019, 1, ["HRINTSTA"])
     @test isdir(@datadep_str "CPS 20191")
     @test Tables.istable(tbl) == true
-    @test in(:HRINTSTA, getfield(tbl, :names))
+    @test in(:hrintsta, getfield(tbl, :names))
     tbl_all = cpsdata(2020,1)
     @test in(:pworwgt, getfield(tbl_all, :names))
 end
